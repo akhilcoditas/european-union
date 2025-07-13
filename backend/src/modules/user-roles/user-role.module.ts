3aad7e6c96a9fbf5b69804ab2db5204805c76334
akhilcoditas <akhil.sachan@coditas.com>
2025-07-13 18:26:59 +0530

    Integrate SharedModule into UserRoleModule for improved modularity

--- backend/src/modules/user-roles/user-role.module.ts
+++ backend/src/modules/user-roles/user-role.module.ts
@@ -3,9 +3,10 @@ import { UserRoleService } from './user-role.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRoleEntity } from './entities/user-role.entity';
 import { UserRoleRepository } from './user-role.repository';
+import { SharedModule } from '../shared/shared.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRoleEntity])],
+  imports: [TypeOrmModule.forFeature([UserRoleEntity]), SharedModule],
   providers: [UserRoleService, UserRoleRepository],
   exports: [UserRoleService],
 })
