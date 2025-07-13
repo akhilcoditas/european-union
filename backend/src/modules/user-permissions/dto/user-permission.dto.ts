69a568815e9ef7fd93f3ffef8e1870c97f1d46ea
akhilcoditas <akhil.sachan@coditas.com>
2025-07-13 15:35:34 +0530

    user isGranted issue fixed

--- backend/src/modules/user-permissions/dto/user-permission.dto.ts
+++ backend/src/modules/user-permissions/dto/user-permission.dto.ts
@@ -26,7 +26,7 @@ export class BulkCreateUserPermissionsDto {
     example: [
       {
         permissionId: '123e4567-e89b-12d3-a456-426614174000',
-        isActive: true,
+        isGranted: true,
       },
     ],
   })
